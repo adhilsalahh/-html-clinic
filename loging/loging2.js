dda238cb25cde0af03660e85879ffd3c8e5dcdd1
adhilsalahh <adhilsalahhk@gmail.com>
2023-12-19 13:35:42 +0530

    Initial commit

---
+++ loging/loging2.js
@@ -0,0 +1,21 @@
+function a(){
+    alert("Registerd succesfullly")
+}
+function c(){
+    var d=confirm("do you want to leave")
+    if(d){      
+    alert("redirecting")
+    window.location="https://www.w3schools.com/tags/tag_data.asp"
+    }
+    else{
+
+        alert("cancelling")
+        window.location=""
+    }
+}
+function e(){
+    window.print("")
+}
+function f(){
+    window.open("datalock.html")
+}
